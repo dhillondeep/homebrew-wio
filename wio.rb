36d83cf3874c546768d854a826ef595287fdde96
wiobot <deep@deepdhillon.ca>
2018-05-28 16:49:40 -0400

    Brew formula update for wio version v0.1.0

--- wio.rb
+++ wio.rb
@@ -1,15 +1,24 @@
 class Wio < Formula
-    desc "Embedded development environment"
-    homepage "https://github.com/dhillondeep/wio"
-    url "https://github.com/dhillondeep/wio/releases/download/v0.1.0/wio_0.1.0_darwin_64-bit.tar.gz"
-    sha256 "01b3639afb5611d7254e04f7f465bb8f948ca4c3f1b42b65ec4a80232fe0d1c4"
-    version "0.1.0"
+  desc "Embedded development environment"
+  homepage "https://github.com/dhillondeep/wio"
+  url "https://github.com/dhillondeep/wio/releases/download/v0.1.0/wio_0.1.0_darwin_64-bit.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  version "0.1.0"
+  sha256 "b28a3c4335ce681f642c164819e7c5aaadda1453e46462260b800ee913dccbb2"
   
-    depends_on "node" "make" "cmake" "osx-cross/avr" "avr-gcc" "avrdude"
-  
-    bottle :unneeded
-  
-    def install
-      bin.install "wio"
-    end
+  depends_on "make"
+  depends_on "cmake"
+  depends_on "node"
+  depends_on "osx-cross/avr"
+  depends_on "avr-gcc"
+  depends_on "avrdude"
+
+  def install
+    bin.install "program"
+    ...
   end
+
+  test do
+    system "#{bin}/wio --version"
+    ...
+  end
+end
