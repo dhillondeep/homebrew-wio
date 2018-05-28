e22c44cf99db8ff1dc7f54e1cbbf3a8fd980d6bd
dhillondeep <deep@deepdhillon.ca>
2018-05-28 17:04:51 -0400

    Brew formula update for wio version v0.1.0

--- wio.rb
+++ wio.rb
@@ -3,12 +3,11 @@ class Wio < Formula
   homepage "https://github.com/dhillondeep/wio"
   url "https://github.com/dhillondeep/wio/releases/download/v0.1.0/wio_0.1.0_darwin_64-bit.tar.gz"
   version "0.1.0"
-  sha256 "925b517219cab4b03a93f4fa2069178e345a33cb277dc882589dadbfad5f971c"
+  sha256 "994b8111319aca9dcfbec2cbae3d2d048226296faae0bfaed9ffcbf2fed8628e"
   
   depends_on "make"
   depends_on "cmake"
   depends_on "node"
-  depends_on "osx-cross/avr"
   depends_on "avr-gcc"
   depends_on "avrdude"
 
