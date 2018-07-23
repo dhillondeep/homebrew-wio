669db8165994a5648f0d2bb0a3996e5c712cad30
dhillondeep <deep@deepdhillon.ca>
2018-07-22 23:14:32 -0400

    Brew formula update for wio version v0.4.0

--- Formula/wio.rb
+++ Formula/wio.rb
@@ -3,7 +3,7 @@ class Wio < Formula
   homepage "https://github.com/wio/wio"
   url "https://github.com/wio/wio/releases/download/v0.4.0/wio_darwin_x86_64.tar.gz"
   version "0.4.0"
-  sha256 "06e8832fb42210afcaace1ec44c1c9a6f90115ffaaa8488928b900a58e8c19ff"
+  sha256 "b724adc1fac7098ac0ec0e2ef2b025941a553353487c58eb748ae45ed80a4fd4"
 
   def install
     prefix.install Dir["toolchain"]
