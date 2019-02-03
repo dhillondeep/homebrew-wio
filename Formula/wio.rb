260eaf85c428a88dd7c91e9c49eb5ded33ff7208
Deep Dhillon <deep@deepdhillon.ca>
2019-02-03 11:36:09 -0500

    Version 0.7.1

--- Formula/wio.rb
+++ Formula/wio.rb
@@ -1,12 +1,11 @@
 class Wio < Formula
   desc "An IOT Development Environment"
   homepage "https://github.com/wio/wio"
-  url "https://github.com/wio/wio/releases/download/v0.6.2/wio_darwin_x86_64.tar.gz"
-  version "0.6.2"
-  sha256 "85e46f3aba2a3d760b5c905d7f1536586baec3dbde2c6e8fd2dd3feeab217f3f"
+  url "https://github.com/wio/wio/releases/download/v0.7.1/wio_darwin_x86_64.tar.gz"
+  version "0.7.1"
+  sha256 "69a1943fd4a02933b9bc4440695b72867825e3fb898d6e4d427988a70200f792"
 
   def install
-    prefix.install Dir["toolchain"]
     File.rename("wio_darwin_x86_64", "wio")
     bin.install "wio"
   end
