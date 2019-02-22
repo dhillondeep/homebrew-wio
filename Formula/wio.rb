f6e4d0b2b69a32f388714165ae3392e7ce059342
dhillondeep <deep@deepdhillon.ca>
2019-02-21 22:36:54 -0500

    Brew formula update for wio version v0.9.0

--- Formula/wio.rb
+++ Formula/wio.rb
@@ -3,7 +3,7 @@ class Wio < Formula
   homepage "https://github.com/wio/wio"
   url "https://github.com/wio/wio/releases/download/v0.9.0/wio_0.9.0_macOS_64bit.tar.gz"
   version "0.9.0"
-  sha256 "82d1d7a7dcc249879e2172a0cbf4f70c9df323d72630ae0884ecfe41495cf014"
+  sha256 "007815ceecf04924b28ee88c074fe161fd3bf960562b5b5eea9fe551a93d792f"
 
   def install
     bin.install "wio"
